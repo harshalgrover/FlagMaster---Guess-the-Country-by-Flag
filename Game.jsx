57dc8d6f40b2b501eff1b68bf4706e77ad141009
Yuvraj Babbar <yuvrajbabbar5@gmail.com>
2026-05-09 02:47:58 +0530

    Add files via upload

--- Game.jsx
+++ Game.jsx
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 import "./Game.css";
 export default function Game({ onExit }) {
   const [countries, setCountries] = useState([]);
+  const [question, setQuestion] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [selected, setSelected] = useState("");
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all?fields=name,flags,cca3")
       .then((res) => res.json())
       .then((data) => {
         setCountries(data);
+        newQuestion(data);
       });
   }, []);
+
+  function newQuestion(data) {
+    setSelected("");
+    const shuffled = [...data].sort(() => Math.random() - 0.5);
+    const opts = shuffled.slice(0, 4);
+    setQuestion(opts[0]);
+    setOptions(opts.sort(() => Math.random() - 0.5));
+  }
+
+  if (!question) {
+    return <h2>Loading...</h2>;
+  }
+
 }
